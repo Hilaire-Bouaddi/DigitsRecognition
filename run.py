8aaf830a6f6f7e2a99d065fd1cef1468cead2746
Hilaire-Bouaddi <hilaire.bouaddi@insa-lyon.fr>
2019-06-14 13:41:35 +0200

    anim

--- run.py
+++ run.py
@@ -1,1 +1,8 @@
+import matplotlib.pyplot as plt
+import animation
 
+fig = plt.figure(figsize=(12, 12))
+ax = fig.gca()
+ax.axis('off')
+animation.draw_neural_net(ax, 0.1, 0.9, 0.1, 0.9, [15,12,10])
+plt.show()
--- validation.py
+++ validation.py
@@ -1,5 +1,4 @@
 import mnist_loader
-import network
 import pickle
 
 training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
